4edad53a9aa0b3eb336f76a9e3a1cf66d1458439
AlbertLin0327 <linhsinkai@gmail.com>
2025-05-04 19:26:38 +0800

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
-  base: '/AlbertLin0327.github.io/',
+  base: '/albertlin0327.github.io/',
   plugins: [react()],
   resolve: {
     alias: {
